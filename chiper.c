69da8b398444c6de58688e880d6e76c66a722e24
Thanatud Boonyaanuchit <73384731+justadaydreamer@users.noreply.github.com>
2022-11-16 20:10:20 +0700

    Add files via upload

---
+++ chiper.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+
+int main(void){
+    char x,y,z,n; 
+    int a;
+    scanf("%c", &x);
+    scanf("%c", &y);
+    scanf("%c",&z);
+    printf("Select to Decode or Encode\n");
+    printf("If Encode press 1 of Decode press 2 :\n");
+    scanf("%d", &a);
+   
+    
+    if (a == 1){
+    x = x+3;
+    y = y+3;
+    z = z+3;
+    
+    printf("Select to encode in word of interger");
+    scanf(" %c", &n);
+    
+    //interger change word
+    if (n == 'i'){
+    printf("%i", x);
+    printf("%i", y);
+    printf("%i", z);
+    }
+    //chiper change word
+    if (n == 'w'){
+    printf("%c", x);
+    printf("%c", y);
+    printf("%c", z);
+    }
+    }
+
+    if (a == 2){
+    x = x-3;
+    y = y-3;
+    z = z-3;
+    
+    printf("Select to encode in word of interger");
+    scanf(" %c", &n); 
+
+    if (n == 'i'){
+    printf("%i", x);
+    printf("%i", y);
+    printf("%i\n", z);
+    } 
+
+    if (n == 'w'){
+    printf("%c", x);
+    printf("%c", y);
+    printf("%c\n", z);
+    } 
+    }
+     
+}
